5e1af3f943a76ea65e7d166c4ed09134c0a8561f
Nonthapat Kimram <nonthapat.kimram@g.swu.ac.th>
2023-05-25 22:00:10 +0700

    First Commit 🐰

---
+++ app/components/Hero.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Image from "next/image";
+import imgP from "../assets/images/img-p.png";
+import imgC from "../assets/images/cricle-letters-1.png";
+
+function Hero() {
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="img-hero w-64 md:w-80 lg:w-1/4">
+        <Image src={imgP} alt="hero-img" />
+      </div>
+
+      <div className="cricle-letters">
+        <Image src={imgC} className="w-128" priority={true} alt="..." />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,150 @@
-import Image from 'next/image'
+"use client";
+import React, { useState, useEffect } from "react";
+import {
+  AnimatePresence,
+  motion,
+} from "framer-motion";
+
+//Page 
+import Hero from "./components/Hero";
+
+import Img1 from "./assets/images/loader/1.jpg"
+import Img2 from "./assets/images/loader/2.jpg"
+import Img3 from "./assets/images/loader/3.jpg"
+import Img4 from "./assets/images/loader/4.jpg"
+import Img5 from "./assets/images/loader/5.jpg"
+import Img6 from "./assets/images/loader/6.jpg"
+import Img7 from "./assets/images/loader/7.jpg"
+import Img8 from "./assets/images/loader/8.jpg"
+import Img9 from "./assets/images/loader/9.jpg"
+import Img10 from "./assets/images/loader/10.jpg"
+
+const imagePaths = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9, Img10]
+
+const len = Object.values(imagePaths).length;
+const imgs: any[] = (Object.values(imagePaths) as unknown as string[]).slice(0, len);
+
+const parentVariants = {
+  removed: {
+    y: "-100%",
+    transition: {
+      y: {
+        type: "tween",
+        duration: 0.3,
+      },
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      delay: 2,
+      delayChildren: 2,
+      staggerChildren: 0.2,
+      type: "tween",
+      duration: 1,
+      opacity: {
+        type: "tween",
+        duration: 0.4,
+      },
+    },
+  },
+};
+
+const childrenVariants = {
+  hidden: {
+    height: "0",
+  },
+  show: {
+    height: "100%",
+    transition: {
+      type: "tween",
+      duration: 0.5,
+    },
+  },
+};
 
 export default function Home() {
+  const [isAnimatingStart, setIsAnimatingStart] = useState<boolean>(false);
+  const [isLandingTime, setIsLandingTime] = useState<boolean>(true);
+
+  useEffect(() => {
+    Promise.all(imgs.map((src) => fetch(src.src))).then(() => {
+      setIsAnimatingStart(true);
+      setTimeout(() => setIsLandingTime(false), 1000);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (isAnimatingStart) {
+      setTimeout(() => {
+        setIsAnimatingStart(false);
+      }, 5000);
+    }
+  }, [isAnimatingStart]);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      {isLandingTime && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={isLandingTime ? { opacity: 1 } : { opacity: 0 }}
+          transition={{ duration: 0.5 }}
+          className="grid place-items-center h-screen"
+        >
+          Loading...
+        </motion.div>
+      //<div className="grid place-items-center h-screen">Loading...</div>
+      )}
+
+      <AnimatePresence>
+        {isAnimatingStart && (
+          <motion.div
+            style={{
+              zIndex: 999,
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+            }}
+            variants={parentVariants}
+            initial="hidden"
+            animate="show"
+            exit="removed"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            {imgs.map((src: any, i) => (
+              <motion.div
+                variants={childrenVariants}
+                key={src.src}
+                style={{
+                  bottom: 0,
+                  overflow: "hidden",
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                }}
+              >
+                <motion.img
+                  style={{
+                    position: "absolute",
+                    width: "100vw",
+                    height: "100vh",
+                    objectFit: "cover",
+                    objectPosition: "center",
+                    bottom: 0,
+                  }}
+                  src={src.src}
+                ></motion.img>
+              </motion.div>
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      {!isAnimatingStart && (
+        <Hero/>
+      )}
+    </>
+  );
 }
