aaa86b42a15f4011f21001a79848bec8e18e2d5d
Nonthapat Kimram <nonthapat.kimram@g.swu.ac.th>
2023-05-26 11:06:49 +0700

    style: update new main-hero 🐰

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,17 +3,31 @@ import Image from "next/image";
 import imgP from "../assets/images/img-p.png";
 import imgC from "../assets/images/cricle-letters-1.png";
 
+import localfont from 'next/font/local'
+const glorifyLight = localfont({ src: '../assets/fonts/glorify_light-webfont.woff2' })
+const glorifyReg = localfont({ src: '../assets/fonts/glorify_regular-webfont.woff2' })
+
 function Hero() {
   return (
-    <div className="grid place-items-center h-screen">
-      <div className="img-hero w-64 md:w-80 lg:w-1/4">
-        <Image src={imgP} alt="hero-img" />
-      </div>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-12 gap-7 mt-14 md:mt-0 lg:mt-0">
+        <div className={`${glorifyReg.className} col-span-12 text-center text-4xl md:text-6xl lg:text-6xl text-color1`}>
+            Korranid <br></br> Laosubinprasoet
+        </div>
 
-      {/* <div className="cricle-letters">
-        <Image src={imgC} alt="..." />
-      </div> */}
+        <div className="col-span-12 max-w-screen-lg mx-auto img-hero w-64 md:w-80 lg:w-96">
+            <Image src={imgP} className="mx-auto" priority={true} alt="hero-img" />
+        </div>
+      </div>
     </div>
+    // <div className="grid grid-cols-12">
+    //     <div className="header">
+    //         <p>Korranid Laosubinprasoet</p>
+    //     </div>
+    //     <div className="img-hero w-64 md:w-80 lg:w-96">
+    //         <Image src={imgP} alt="hero-img" />
+    //     </div>
+    // </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-
       spacing: {
         '128': '32rem',
+      },
+
+      colors: {
+        'color1': '#7380a8'
       }
     },
   },
