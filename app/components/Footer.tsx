dd3b424496f3aebdf613cfaa680b0bebc5718185
Nonthapat Kimram <nonthapat.kimram@g.swu.ac.th>
2023-06-07 20:49:13 +0700

    update: logo size footer & new cover web ✨🐰

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -14,12 +14,12 @@ function Footer() {
   return (
     <footer className="container-fluid mx-auto pt-4 ps-4 pe-4 pb-10 aheye-block-footer text-black/50">
       <div className="grid grid-cols-0 gap-8">
-        <div><Image src={rabbit} className="mx-auto w-20" alt="rabbit" /></div>
+        <div><Image src={rabbit} className="mx-auto w-14" alt="rabbit" /></div>
         <div className={`${noto_S_TH.className} text-center`}>
-            เว็บไซต์นี้เป็นส่วนหนึ่งของโปรเจควันเกิดของ <br/> อ๊ะอาย กรณิศ เล้าสุบินประเสริฐ <br/> ศิลปินวง 4EVE, นักร้อง, นักแสดง และนักพากย์ <br/> 9 มิถุนายน 2566
+          เว็บไซต์นี้เป็นส่วนหนึ่งของโปรเจควันเกิดของ <br/> อ๊ะอาย กรณิศ เล้าสุบินประเสริฐ <br/> ศิลปินวง 4EVE, นักร้อง, นักแสดง และนักพากย์ <br/> 9 มิถุนายน 2566
         </div>
         <div className={`${noto_S_TH.className} text-center`}>
-            This website is a part of <br/> Aheye | Korranid Laosubinprasoet's Brithday Project <br/> Artist of 4EVE, Singer, Actor and Voice actor <br/> June 9, 2023
+          This website is a part of <br/> Aheye | Korranid Laosubinprasoet's Brithday Project <br/> Artist of 4EVE, Singer, Actor and Voice actor <br/> June 9, 2023
         </div>
       </div>
     </footer>
